fe6a2c3a3f2f8ff6c06a25e1be9f6f228686d10d
Cindi <cindi.thornton@gmail.com>
2021-05-19 22:46:50 +1000

    Created concept of Foe Fighters to better capture the hexes on a foe. Updated code and results text to use this info

---
+++ Classes/FoeFighter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPWUHexingTrainer.Classes
+{
+    public class FoeFighter
+    {
+        public Foe Foe { get; set; }
+        public string FoeName { get; set; }
+        public string FoeNameWithHexes { get; set; }
+        public string FoughtBy { get; set; }
+        public List<HexType> Hexes { get; set; }
+
+        //public List<HexInfo> HexInfos { get; set; }
+        //public string HexedBy { get; set; }
+
+        public FoeFighter(Foe f)
+        {
+        }
+
+        public FoeFighter(Foe foe, string foeName, List<HexType> hexes, string foughtBy)
+        {
+            Foe = foe;
+            FoeName = foeName;
+            FoughtBy = foughtBy;
+            Hexes = hexes;
+        }
+
+        public FoeFighter(Foe foe, string foeName, List<HexType> hexes)
+        {
+            Foe = foe;
+            Hexes = hexes;
+        }
+
+        public FoeFighter(Foe foe, string foeName, string foughtBy)
+        {
+            Foe = foe;
+            FoeName = foeName;
+            FoughtBy = foughtBy;
+            Hexes = new List<HexType>();
+        }
+
+        public FoeFighter(Foe foe, string foeName)
+        {
+            Foe = foe;
+            FoeName = foeName;
+            Hexes = new List<HexType>();
+        }
+
+        //public struct HexInfo
+        //{
+        //    public HexType HexType { get; set; }
+        //    public string By { get; set; }
+
+        //    public HexInfo(HexType hexType, string by)
+        //    {
+        //        HexType = hexType;
+        //        By = by;
+        //    }
+        }
+    
+}
